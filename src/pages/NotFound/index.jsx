e8e32845acecf3cf048560567b26b6d629d2d199
jalehjaleh28 <94356744+jalehjaleh28@users.noreply.github.com>
2022-08-07 20:48:29 +0200

    edit NotFound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import "./NotFound.css"
 
 export const Notfound = () => {
   return (
-    <div>Notfound</div>
+    <div className='not-found-container' >
+      <h1>This page not found!</h1>
+    </div>
   )
 }
